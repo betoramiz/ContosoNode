12dcf7bd34f9711a237a6808f5763392d6907ebe
Manuel Alberto Ramírez <maramirez@qcsoluciones.local>
2018-03-15 10:36:05 -0600

    agregados data de prueba

---
+++ models/course.js
@@ -0,0 +1,27 @@
+let courses = [
+    {
+        "Id" : 1,
+        "Name" : "Calculo Diferencial"        
+    },
+    {
+        "Id" : 2,
+        "Name" : "Calculo Integral"        
+    },
+    {
+        "Id" : 3,
+        "Name" : "Fisica - Estatica"        
+    },{
+        "Id" : 4,
+        "Name" : "Fisica - Dinamica"        
+    },
+    {
+        "Id" : 5,
+        "Name" : "Diseño de Software"        
+    },{
+        "Id" : 6,
+        "Name" : "Programacion"        
+    }
+    
+];
+
+module.exports.courses = courses;
---
+++ models/student.js
@@ -0,0 +1,25 @@
+let students = [
+    {
+        "Id" : 1,
+        "Name" : "Alberto",
+        "LastName" : "Ramirez",
+        "Grade": 1,
+        "Courses" : []
+    },
+    {
+        "Id" : 2,
+        "Name" : "Jose",
+        "LastName" : "Perez Leon",
+        "Grade": 5,
+        "Courses" : []
+    },
+    {
+        "Id" : 3,
+        "Name" : "Atanacio",
+        "LastName" : "Rodriguez",
+        "Grade": 3,
+        "Courses" : []
+    }
+];
+
+module.exports.students = students;
