2540d0844ad5832fb367475cf5dae9863de35aff
Manuel Alberto Ramírez <maramirez@qcsoluciones.local>
2018-03-15 17:36:57 -0600

    vista students

--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const route = express.Router();
 
+let students = require('../models/student').students;
+
 route.get('/', (req, res) => {
-    res.render('./students/list');
+    console.log(students);
+    res.render('./students/list', { students : students });
 });
 
 module.exports = route;
