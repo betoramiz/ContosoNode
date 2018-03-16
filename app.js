a842df4c61728e8127b637dc9b531586ee2ff424
Manuel Alberto Ramírez <maramirez@qcsoluciones.local>
2018-03-16 14:57:10 -0600

    se agrego body parser

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+//body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true}));
+
 //view engine
 var exphbs  = require('express-handlebars');
 const hbs = exphbs.create( { defaultLayout: 'index', layoutsDir : __dirname + '/views', extname : 'hbs'} );
--- routes/student.js
+++ routes/student.js
@@ -7,4 +7,13 @@ route.get('/', (req, res) => {
     res.render('./students/list', { students : students });
 });
 
+
+route.get('/add', (req, res) => {
+    res.render('./students/add');
+});
+
+route.post('/add', (req, res) => {
+    res.send("Add Student to student list");    
+});
+
 module.exports = route;
